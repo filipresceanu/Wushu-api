524ee277541db24b120f796d43c59c84d4a0516f
filipresceanu <82608145+filipresceanu@users.noreply.github.com>
2024-03-14 12:20:48 +0200

    Delete WushuIdentity directory

--- WushuIdentity/Configurations/JwtConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WushuIdentity.Configurations
-{
-    public class JwtConfig
-    {
-        public string Secret { get; set; }
-        public TimeSpan ExpirationTime { get; set; }
-    }
-}
